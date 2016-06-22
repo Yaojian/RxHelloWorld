57ad03c3b0a6aa517967956d70ed4a02aa9b785f
Yaojian <skyaoj@gmail.com>
2016-06-22 17:28:44 +0800

    Add NUnit 2.6.4 and RxHelloWorldTests.cs

---
+++ RxHelloWorld/RxHelloWorldTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace RxHelloWorld
+{
+    [TestFixture]
+    public class RxHelloWorldTests
+    {
+        [Test]
+        public void NUnit_should_be_launched_successfully()
+        {
+            Console.WriteLine("NUnit is launched successfully.");
+            Assert.IsTrue(true);
+        }
+    }
+}
+
